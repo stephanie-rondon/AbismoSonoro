9bde865a1f1283fd3a39c270c84efb7241a0887e
stephanie-rondon <stephanie.rondon@escola.pr.gov.br>
2026-07-02 11:54:31 -0300

    Add files via upload

--- Home/script.js
+++ Home/script.js
@@ -2,18 +2,17 @@ const searchInput = document.getElementById('search');
 
 searchInput.addEventListener('input', (event) => {
     const value = formatString(event.target.value);
-
     const items = document.querySelectorAll('.items .item');
 
     items.forEach(item => {
         if(formatString(item.textContent).indexOf(value) !== -1) {
-            item.computedStyleMap.display = 'flex';
+            item.style.display = 'flex'; 
         } else {
-            item.computedStyleMap.display = 'none';
+            item.style.display = 'none';
         }
-    })
+    });
 });
 
 function formatString(value) {
-    return value.toLowerCase().trim();
+    return value.toLowerCase().trim().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 }
