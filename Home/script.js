d2bc99637b08d2e5d7b00286d2548c524b460cc3
GiovanniXavierSOS <giovanni.davila@escola.pr.gov.br>
2026-07-02 12:06:12 -0300

    Add files via upload

--- Home/script.js
+++ Home/script.js
@@ -1,17 +1,20 @@
 const searchInput = document.getElementById('search');
 
+
 searchInput.addEventListener('input', (event) => {
     const value = formatString(event.target.value);
     const items = document.querySelectorAll('.items .item');
 
+
     items.forEach(item => {
         if(formatString(item.textContent).indexOf(value) !== -1) {
-            item.style.display = 'flex'; 
+            item.style.display = 'flex';
         } else {
             item.style.display = 'none';
         }
     });
 });
+
 
 function formatString(value) {
     return value.toLowerCase().trim().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
